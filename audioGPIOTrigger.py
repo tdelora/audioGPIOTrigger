d8c2596b32d94acb0fe28a82d681ad209ea3a010
Thomas E. De Lora <tedelora21@gmail.com>
2025-08-31 20:41:35 -0700

    Added code to keep the recordings from repeating.

--- audioGPIOTrigger.py
+++ audioGPIOTrigger.py
@@ -4,8 +4,8 @@ import os, random, signal, sys, time, vlc
 # Set pin numbering mode
 GPIO.setmode(GPIO.BCM)
 
-# This script triggers a recording to be played via VLC on a Raspberry Pi
-# when a GPIO pin (default to pin 16 via variable gpioPin) goes to a high state.
+# This script triggers a recording to be playeed vis VLC on a Raspberry Pi
+# when a GPIO pin (default to pin 16 via variable gpioPi) goes to a high state.
 # When triggered it randomly selects and plays a file from a directory specified at startup.
 #
 # Please see project README.md for use cases.
@@ -21,6 +21,10 @@ audioLevel = 100
 
 # Define the GPIO pin you want to monitor
 gpioPin = 16
+
+# Keep track of the last recording playe so we don't repeat.
+# Start with -1 because that number is impossible after initialization.
+lastRand = -1
 
 # Set the pin, using GPIO.PUD_DOWN in GPIO.setup should keep the pin state low
 # at startup and when not triggered.
@@ -47,7 +51,7 @@ try:
     # audio directory to be updated and utilized  without restarting this
     # script.
 
-    global audioDirectory
+    global audioDirectory, lastRand
 
     dirContents = os.listdir(audioDirectory)
 
@@ -66,7 +70,15 @@ try:
     # From trimmed {dirContents} randomly select an audio file to play
     contentNum = len(dirContents)
     # print(f"contentNum: {contentNum}")
-    randSel = random.randint(0, (contentNum - 1))
+
+    # Get a random value and check it against the last random value.
+    # Continue if the values match, this keeps us from playing the same recordings
+    # over and over again.
+    while (randSel := random.randint(0, contentNum - 1)) == lastRand:
+      pass
+
+    # Keep track of the random value selected.
+    lastRand = randSel
 
     # Set the media specification for the VLC player and play the audio.
     media = vlcInstance.media_new_path(dirContents[randSel])
